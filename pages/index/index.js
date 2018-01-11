cb7cb2167db3bc72d3f788c6d9a30a32d821d6e3
liunian1010 <376921416@qq.com>
2018-01-11 16:38:42 +0800

    Signed-off-by: liunian1010  附带demo源码

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,8 +11,13 @@ Page({
   },
   //事件处理函数
   bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+    // wx.navigateTo({
+    //   url: '../logs/logs'
+    // })
+    wx.scanCode({
+      success: (res) => {
+        console.log(res)
+      }
     })
   },
   onLoad: function () {
